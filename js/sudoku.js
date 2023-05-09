bb8483388bfa8d808a971a70d245233ca068cd35
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2023-05-09 20:59:18 +0300

    .

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,12 +1,12 @@
-function turningToString(grid){
+function turningToString(grid) {
     let myArray = [];
-    for (let i = 0 ;i < 9 ;i++){
+    for (let i = 0; i < 9; i++) {
         myArray[i] = [];
-        for (let j = 0 ;j < 9;j++){
-            if (grid[i][j] === 0){
+        for (let j = 0; j < 9; j++) {
+            if (grid[i][j] === 0) {
                 myArray[i][j] = '';
-            }else {
-            myArray[i][j] = grid[i][j].toString();
+            } else {
+                myArray[i][j] = grid[i][j].toString();
             }
         }
     }
@@ -19,266 +19,267 @@ function generateSudoku() {
     for (let i = 0; i < 9; i++) {
         grid[i] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     }
-    
+
     // Fill the diagonal sub-grids with random numbers
     fillDiagonalSubGrids(grid);
-    
+
     // Fill the remaining cells
     fillRemainingCells(grid, 0, 3);
-    
+
     // Remove some numbers to create the puzzle
     removeNumbers(grid);
-    
-    let myArray =  turningToString(grid);
-    
-    
+
+    let myArray = turningToString(grid);
+
+
     return myArray;
-    }
-    
+}
+
 function fillDiagonalSubGrids(grid) {
     for (let i = 0; i < 9; i += 3) {
         this.fillSubGrid(grid, i, i);
     }
-    }
-    
+}
+
 function fillSubGrid(grid, row, col) {
     let nums = this.shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    
+
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-        grid[row + i][col + j] = nums.pop();
+            grid[row + i][col + j] = nums.pop();
         }
     }
-    }
-    
+}
+
 function shuffle(array) {
-        for (let i = array.length -1; i >0 ;i--){
-            let j = Math.floor(Math.random() * (i+1));
-            [array[i],array[j]] = [array[j],array[i]];
-        }
-        return array;
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
-    
-function fillRemainingCells(grid,row,col){
-        if(col>=9 && row<8){
-            row=row+1;
-            col=0;
+    return array;
+}
+
+function fillRemainingCells(grid, row, col) {
+    if (col >= 9 && row < 8) {
+        row = row + 1;
+        col = 0;
+    }
+    if (row >= 9 && col >= 9) {
+        return true;
+    }
+    if (row < 3) {
+        if (col < 3) {
+            col = 3;
         }
-        if(row>=9 && col>=9){
+    } else if (row < 6) {
+        if (col === parseInt(row / 3) * 3) {
+            col += 3;
+        }
+    } else {
+        if (col === 6) {
+            row += 1;
+            col = 0;
+            if (row >= 9) {
+                return true;
+            }
+        }
+    }
+
+    for (let num = 1; num <= 9; num++) {
+        if (this.isSafe(grid, row, col, num)) {
+            grid[row][col] = num;
+            if (this.fillRemainingCells(grid, row, col + 1)) {
+                return true;
+            }
+            grid[row][col] = 0;
+        }
+    }
+    return false;
+}
+
+function isSafe(grid, row, col, num) {
+    return !this.usedInRow(grid, row, num) && !this.usedInCol(grid, col, num) && !this.usedInBox(grid, row - row % 3, col - col % 3, num);
+}
+
+function usedInRow(grid, row, num) {
+    for (let col = 0; col < 9; col++) {
+        if (grid[row][col] === num) {
             return true;
         }
-        if(row<3){
-            if(col<3){
-                col=3;
-            }
-        }else if(row<6){
-            if(col===parseInt(row/3)*3){
-                col+=3;
-            }
-        }else{
-            if(col===6){
-                row+=1;
-                col=0;
-                if(row>=9){
-                    return true;
-                }
-            }
-        }
-        
-        for(let num=1;num<=9;num++){
-            if(this.isSafe(grid,row,col,num)){
-                grid[row][col]=num;
-                if(this.fillRemainingCells(grid,row,col+1)){
-                    return true;
-                }
-                grid[row][col]=0;
-            }
-        }
-        return false;
     }
-    
-function isSafe(grid,row,col,num){
-        return !this.usedInRow(grid,row,num) && !this.usedInCol(grid,col,num) && !this.usedInBox(grid,row-row%3,col-col%3,num);
-    }
-    
-function  usedInRow(grid,row,num){
-        for(let col=0;col<9;col++){
-            if(grid[row][col]===num){
-                return true;
-            }
-        }
-        return false;
-    }
-    
-function usedInCol(grid,col,num){
-        for(let row=0;row<9;row++){
-            if(grid[row][col]===num){
-                return true;
-            }
-        }
-        return false;
-    }
-    
-function usedInBox(grid,startRow,startCol,num){
-        for(let row=0;row<3;row++){
-            for(let col=0;col<3;col++){
-                if(grid[row+startRow][col+startCol]===num){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    
-function removeNumbers(grid) {
-        let count = Math.floor(Math.random() * (55 -45)) +45 ;
-        while(count!=0){
-            let cellId = Math.floor(Math.random()*81);
-            let row = Math.floor(cellId/9);
-            let col = cellId%9;
-            while(grid[row][col]==0){
-                cellId = Math.floor(Math.random()*81);
-                row = Math.floor(cellId/9);
-                col = cellId%9;
-            }
-            let backup = grid[row][col];
-            grid[row][col]=0;
-            count--;
-        } 
+    return false;
 }
-class Sudoku extends GameEngine{
+
+function usedInCol(grid, col, num) {
+    for (let row = 0; row < 9; row++) {
+        if (grid[row][col] === num) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function usedInBox(grid, startRow, startCol, num) {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (grid[row + startRow][col + startCol] === num) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function removeNumbers(grid) {
+    let count = Math.floor(Math.random() * (55 - 45)) + 45;
+    while (count != 0) {
+        let cellId = Math.floor(Math.random() * 81);
+        let row = Math.floor(cellId / 9);
+        let col = cellId % 9;
+        while (grid[row][col] == 0) {
+            cellId = Math.floor(Math.random() * 81);
+            row = Math.floor(cellId / 9);
+            col = cellId % 9;
+        }
+        let backup = grid[row][col];
+        grid[row][col] = 0;
+        count--;
+    }
+}
+class Sudoku extends GameEngine {
     //place your js code here
-    constructor(){
-        
-        let arr0= generateSudoku();
+    constructor() {
+
+        let arr0 = generateSudoku();
         let arr1 = JSON.parse(JSON.stringify(arr0));
         let arr = [];
         arr[0] = arr0;
         arr[1] = arr1;
         super(arr)
     }
-    
-    
-    
+
+
+
     // we need the code to generator to the initial state
-    
-    
+
+
 
     // rc n (rowcolumn number)or rc r (rowcolumn remove)
-    checkRow_Col_box(myArray,input){
-    
-        let row = parseInt(input.charAt(0)) -1
-        let col = parseInt(input.charAt(1)) -1
+    checkRow_Col_box(myArray, input) {
+
+        let row = parseInt(input.charAt(0)) - 1
+        let col = parseInt(input.charAt(1)) - 1
         let x = input.charAt(3);
         // col and row check
-        for (let i = 0 ;i < 9 ;i++){
-            if (myArray[row][i] === x ){
+        for (let i = 0; i < 9; i++) {
+            if (myArray[row][i] === x) {
                 alert('is found in row')
                 return false;
             }
-            if (myArray[i][col] === x ){
+            if (myArray[i][col] === x) {
                 alert('is found in col')
                 return false;
             }
         }
         // box check detecting the box
-        let boxr = Math.floor(row/3)*3
-        let boxc = Math.floor(col/3)*3
-        for (let i = boxr ;i < boxr+3 ; i++){
-            for (let j = boxc ;j < boxc+3 ; j++){
-                if (myArray[i][j] === x){
+        let boxr = Math.floor(row / 3) * 3
+        let boxc = Math.floor(col / 3) * 3
+        for (let i = boxr; i < boxr + 3; i++) {
+            for (let j = boxc; j < boxc + 3; j++) {
+                if (myArray[i][j] === x) {
                     alert('is found in box')
                     return false;
                 }
             }
         }
-        return true ;
+        return true;
     }
-    input_validity_removing(myArray,input , arr0){
-        if(input.length !== 4){
+    input_validity_removing(myArray, input, arr0) {
+        if (input.length !== 4) {
             alert('the format should be of length 4 ')
             return myArray
         }
-        let row = parseInt(input.charAt(0)) -1
-        let col = parseInt(input.charAt(1)) -1
+        let row = parseInt(input.charAt(0)) - 1
+        let col = parseInt(input.charAt(1)) - 1
         console.log(row)
         console.log(col)
-    
+
         let x = input.charAt(3);
-        if ((row < 0 || row > 8 || col < 0 || col > 8)){
+        if ((row < 0 || row > 8 || col < 0 || col > 8)) {
             alert('not valid input!');
-            return myArray ;
+            return myArray;
         }
         // valid row and column
-        if (x === 'r'){
+        if (x === 'r') {
             // the part for removing
-            if (arr0[row][col] === ''){
+            if (arr0[row][col] === '') {
                 myArray[row][col] = ''
                 console.log('correct remove')
                 console.log(myArray);
                 return myArray;
             }
-            else{alert('the position can\'t be removed')
+            else {
+                alert('the position can\'t be removed')
                 console.log(arr0);
-                return myArray ;
+                return myArray;
             }
-        }else {
+        } else {
             let x_v = parseInt(x);
             console.log(x_v);
-            if ((x_v < 0 || x_v > 9 )){
+            if ((x_v < 0 || x_v > 9)) {
                 alert('not avaliable value');
-                return myArray ;
-            }else{
-                if (myArray[row][col] === ''){
+                return myArray;
+            } else {
+                if (myArray[row][col] === '') {
                     if (this.checkRow_Col_box(myArray
-                        ,input)){
-                    myArray[row][col] = x
-                }
+                        , input)) {
+                        myArray[row][col] = x
+                    }
                 }
             }
         }
-    return myArray ;
-        
+        return myArray;
+
     }
     // after taking the input -- from it to make the index from 1 in the board
-    controller(state , input){
-        let myArray = state [0];
-        let arr0 = state [1];
-        this.input_validity_removing(myArray,input , arr0);
-    
+    controller(state, input) {
+        let myArray = state[0];
+        let arr0 = state[1];
+        this.input_validity_removing(myArray, input, arr0);
+
         return myArray;
     }
-    
+
     // it is ok but we need to add the css
-    drawer(state){
+    drawer(state) {
         let myArray = state[0];
         let myElement = document.getElementById('my-element');
         myElement.innerHTML = '';
         // create a table element
         let table = document.createElement('table');
-        
+
         // iterate over the rows in the array
         for (let i = 0; i < myArray.length; i++) {
-          // create a new row
-        let row = document.createElement('tr');
-        
-          // iterate over the columns in the current row
-        for (let j = 0; j < myArray[i].length; j++) {
-        // create a new cell
-        let cell = document.createElement('td');
-        
-        // set the cell's text content to the current element in the array
-        
-        cell.textContent = myArray[i][j];
-        
-        // append the cell to the row
-        row.appendChild(cell);
-        }
-        // append the row to the table
-        table.appendChild(row);   
+            // create a new row
+            let row = document.createElement('tr');
+
+            // iterate over the columns in the current row
+            for (let j = 0; j < myArray[i].length; j++) {
+                // create a new cell
+                let cell = document.createElement('td');
+
+                // set the cell's text content to the current element in the array
+
+                cell.textContent = myArray[i][j];
+
+                // append the cell to the row
+                row.appendChild(cell);
+            }
+            // append the row to the table
+            table.appendChild(row);
         }
         // append the table to the page
         document.body.appendChild(table);
-        };
-    
+    };
+
 }
--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,83 +1,84 @@
-class TicTacToe extends GameEngine{
-    
-    constructor(){
-        let state  = [
-            ['', '',''],
-            ['', '',''],
-            ['', '','']
+class TicTacToe extends GameEngine {
+
+    constructor() {
+        let state = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
         ];
         super(state);
     }
 
-checkFirstTwoCharacters(input) {
-    let firstChar = input.charAt(0);
-    let secondChar = input.charAt(1);
-    if (input.length > 2){
-        return false;
-    }
-    if ((firstChar === '1' || firstChar === '2' || firstChar === '3') && (secondChar === '1' || secondChar === '2' || secondChar === '3')) {
-        return true;
-    } else {
-        return false;
-    }
-}
-controller (state,input ){
-    
-    const turns=['x','o']
-    let turn;
-    if (state.turn){
-        turn=turns[0]
-    }else{
-        turn=turns[1]
-    }
-    
-    
-    // console.log(parseInt(input.charAt(0)));
-    if (this.checkFirstTwoCharacters(input)){
-        if (state[parseInt(input.charAt(0))-1][parseInt(input.charAt(1))-1]==='')
-        {state[parseInt(input.charAt(0))-1][parseInt(input.charAt(1))-1]=turn;
-        state.turn = !state.turn ;}else{
-            alert('this position is not empty')
+    checkFirstTwoCharacters(input) {
+        let firstChar = input.charAt(0);
+        let secondChar = input.charAt(1);
+        if (input.length > 2) {
+            return false;
         }
-    }else{
-        alert('Not valid input!')
+        if ((firstChar === '1' || firstChar === '2' || firstChar === '3') && (secondChar === '1' || secondChar === '2' || secondChar === '3')) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    return state ;
-}
+    controller(state, input) {
 
-drawer(myArray){
-// create a table element
-let myElement = document.getElementById('my-element');
-myElement.innerHTML = '';
-let table = document.createElement('table');
+        const turns = ['x', 'o']
+        let turn;
+        if (state.turn) {
+            turn = turns[0]
+        } else {
+            turn = turns[1]
+        }
 
 
-// iterate over the rows in the array
-for (let i = 0; i < myArray.length; i++) {
-  // create a new row
-let row = document.createElement('tr');
+        // console.log(parseInt(input.charAt(0)));
+        if (this.checkFirstTwoCharacters(input)) {
+            if (state[parseInt(input.charAt(0)) - 1][parseInt(input.charAt(1)) - 1] === '') {
+                state[parseInt(input.charAt(0)) - 1][parseInt(input.charAt(1)) - 1] = turn;
+                state.turn = !state.turn;
+            } else {
+                alert('this position is not empty')
+            }
+        } else {
+            alert('Not valid input!')
+        }
+        return state;
+    }
 
-  // iterate over the columns in the current row
-for (let j = 0; j < myArray[i].length; j++) {
-// create a new cell
-let cell = document.createElement('td');
+    drawer(myArray) {
+        // create a table element
+        let myElement = document.getElementById('my-element');
+        myElement.innerHTML = '';
+        let table = document.createElement('table');
 
-// set the cell's text content to the current element in the array
 
-cell.textContent = myArray[i][j];
-if (myArray[i][j] === 'x'){
-    cell.className='x'
-}else if (myArray[i][j] === 'o'){
-    cell.className='o'
-}
+        // iterate over the rows in the array
+        for (let i = 0; i < myArray.length; i++) {
+            // create a new row
+            let row = document.createElement('tr');
 
-// append the cell to the row
-row.appendChild(cell);
-}
-// append the row to the table
-table.appendChild(row);   
-}
-// append the table to the page
-document.body.appendChild(table);
-};
+            // iterate over the columns in the current row
+            for (let j = 0; j < myArray[i].length; j++) {
+                // create a new cell
+                let cell = document.createElement('td');
+
+                // set the cell's text content to the current element in the array
+
+                cell.textContent = myArray[i][j];
+                if (myArray[i][j] === 'x') {
+                    cell.className = 'x'
+                } else if (myArray[i][j] === 'o') {
+                    cell.className = 'o'
+                }
+
+                // append the cell to the row
+                row.appendChild(cell);
+            }
+            // append the row to the table
+            table.appendChild(row);
+        }
+        // append the table to the page
+        document.body.appendChild(table);
+    };
 }
